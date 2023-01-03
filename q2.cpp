c8baa5ead23c913dd7e99618fa8ef280e834e747
DivingDev <anandrudra2002@gmail.com>
2023-01-04 00:12:39 +0530

    question2 done

---
+++ q2.cpp
@@ -0,0 +1,36 @@
+// Q2 Accepted
+
+// https://leetcode.com/problems/combination-sum-iii/submissions/870640957/
+
+// Code:-
+/*
+class Solution {
+    private:
+    void combination(int idx,vector<int> &v,int n,vector<vector<int>> &ans,vector<int> &ds,int k)
+    {
+        if(idx==v.size())
+        {
+            if(n==0 && k==0)
+            {
+                ans.push_back(ds);
+            }
+            return;
+        }
+        if(v[idx]<=n)
+        {
+            ds.push_back(v[idx]);
+            combination(idx+1,v,n-v[idx],ans,ds,k-1);
+            ds.pop_back();
+        }
+        combination(idx+1,v,n,ans,ds,k);
+    }
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int>v={1,2,3,4,5,6,7,8,9};
+        vector<vector<int>>ans;
+        vector<int>ds;
+        combination(0,v,n,ans,ds,k);
+        return ans;
+    }
+};
+*/
