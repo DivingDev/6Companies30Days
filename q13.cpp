86c664ee530504ee72af015bbd3f7bf4004b01e1
DivingDev <anandrudra2002@gmail.com>
2023-01-05 22:20:24 +0530

    Q13 done

---
+++ q13.cpp
@@ -0,0 +1,13 @@
+// Accepted link
+// https://leetcode.com/problems/airplane-seat-assignment-probability/submissions/872051601/
+
+// code:-
+/*
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        if(n==1)return 1;
+        return 1.0/2;
+    }
+};
+*/
