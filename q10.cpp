658bfc055162e4b84369c41f291c0fe05a6a772f
DivingDev <anandrudra2002@gmail.com>
2023-01-05 16:08:17 +0530

    Q10 done

---
+++ q10.cpp
@@ -0,0 +1,21 @@
+// Accepted link
+// https://leetcode.com/problems/shortest-unsorted-continuous-subarray/submissions/871784116/
+
+// code:-
+/*
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int len = nums.size() - 1, left = -1, right = -1,
+            maxN = nums[0], minN = nums[len];
+        for (int i = 1; i <= len; i++) {
+            int a = nums[i], b = nums[len-i];
+            if (a < maxN) right = i;
+            else maxN = a;
+            if (b > minN) left = i;
+            else minN = b;
+        }
+        return max(0, left + right - len + 1);
+    }
+};
+*/
